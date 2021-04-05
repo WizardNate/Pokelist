709bc8e5f3161c1ff6322a6cc70079bd4d4e899b
Nathan Gulley <76852172+WizardNate@users.noreply.github.com>
2021-04-04 20:12:56 -0400

    Update 0.02: Corrections and Appearance Changes
    Changed mode for "pokelist.sh" to executable, not having done it yet before.
    
    Cleared text after execution of the app, as well as a better looking "overall list" after entering your caught Pokemon.

--- pokelist.sh
+++ pokelist.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+clear
 echo "Hello! What Pokemon did you catch?"
 read pokemon
 echo "$pokemon" >>pokelist.txt
+clear
+echo "Pokemon you have caught so far:"
 echo "-------------------"
 cat pokelist.txt
